f4a188f844936edabc55f011cb8d347be978dc4b
Sabbir Ahmmed <engineersabbir.dev@gmail.com>
2024-11-07 20:53:39 +0600

    jproblem-8.ts file added

---
+++ src/problem-8.ts
@@ -0,0 +1,22 @@
+{
+    //Create a function validateKeys(userData: T, keys: (keyof T)[]) that takes an object obj and an array of keys keys. The function should return true if all of the specified keys exist in the object; otherwise, it should return false.
+
+    const validateKeys = <T extends object>(userObj: T, keys: (keyof T)[]): boolean =>{
+        for (const key of keys) {
+            if (!(key in userObj)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    const user = {
+        name: 'sabbir',
+        age: 23,
+        address: 'shantinagar'
+    }
+    
+    
+    const validateKeysResult = validateKeys(user, ['name', 'age', 'profession'])
+    // console.log(validateKeysResult);
+}
